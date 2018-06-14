c82c5dad224e0a114e381e19003cd7f63d99105e
Randall <randall_pittman@bellsouth.net>
2018-06-14 19:25:29 +0000

    traces

--- scripts/tf_cnn_benchmarks/profile.sh
+++ scripts/tf_cnn_benchmarks/profile.sh
@@ -2,12 +2,11 @@
 
 #nvprof --unified-memory-profiling off --metrics sm_efficiency python3 tf_cnn_benchmarks.py --num_gpus=1 --batch_size=32 --model=resnet50 --variable_update=parameter_server
 
-mkdir -p train_dir
-
+VARIABLE_UPDATE=parameter_server
 python3 tf_cnn_benchmarks.py --data_format=NCHW --batch_size=32 \
-       --model=resnet50 --optimizer=momentum --variable_update=replicated \
-       --gradient_repacking=1 --num_gpus=8 \
-       --num_epochs=1 --weight_decay=1e-4 --use_fp16 \
-       --train_dir=./train_dir --display_every=1
-
-#--trace_file=1X_K80_resnet50.json 
+       --model=resnet50 --optimizer=momentum --variable_update=$VARIABLE_UPDATE \
+       --gradient_repacking=8 --num_gpus=8 \
+       --num_batches=10 --weight_decay=1e-4 --use_fp16 \
+       --display_every=1 --local_parameter_device=cpu \
+       --trace_file=traces/var_update_${VARIABLE_UPDATE}_cpu_K80.json \
+#       --train_dir=./train_dir
