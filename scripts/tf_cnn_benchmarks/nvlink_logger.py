50063cd661ac6085ca886789ec2f7dc52ce68a90
Randall <randall_pittman@bellsouth.net>
2018-06-15 13:01:17 +0000

    upgrades

--- scripts/tf_cnn_benchmarks/nvlink_logger.py
+++ scripts/tf_cnn_benchmarks/nvlink_logger.py
@@ -1,5 +1,4 @@
-import os
-import time
+import os, sys, time
 
 def get_link_util():
     total_rx_kb = 0
@@ -19,7 +18,19 @@ def get_link_util():
     return avg_rx_mb, avg_tx_mb
 
 if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        delay = 1
+    else:
+        delay = float(sys.argv[1])
+    start_time = None
     while True:
         rx_mb, tx_mb = get_link_util()
-        print(time.time(), rx_mb, tx_mb)
-        time.sleep(1)
+        curr_time = time.time()
+        if start_time == None:
+            start_time = curr_time
+            elapsed = 0
+        else:
+            elapsed = curr_time - start_time
+        print("%f,%f,%f" % (elapsed, rx_mb, tx_mb))
+        sys.stdout.flush()
+        time.sleep(delay)
--- scripts/tf_cnn_benchmarks/profile.sh
+++ scripts/tf_cnn_benchmarks/profile.sh
@@ -4,23 +4,20 @@
 
 echo "Resetting nvlink counters"
 nvidia-smi nvlink -sc 0bz > /dev/null;
-echo "Starting background logger"
+echo "Starting background nvlink logger"
+if [ ! -d nvlink_data ]; then
+    mkdir nvlink_data;
+fi
 
-# logger() {
-#     while true; do
-# 	nvidia-smi nvlink -g 0;
-# 	sleep 1;
-#     done
-# }
+VARIABLE_UPDATE=parameter_server
+NVLINK_FILE=nvlink_data/${VARIABLE_UPDATE}.csv
+python3 nvlink_logger.py 0.5 > $NVLINK_FILE &
 
-python3 nvlink_logger.py > test.txt &
-
-VARIABLE_UPDATE=replicated
 #nvprof --unified-memory-profiling off --metrics nvlink_transmit_throughput \
 python3 tf_cnn_benchmarks.py --data_format=NCHW --batch_size=256 \
        --model=resnet50 --optimizer=momentum --variable_update=$VARIABLE_UPDATE \
        --gradient_repacking=4 --num_gpus=4 \
-       --num_batches=50 --weight_decay=1e-4 --use_fp16 \
+       --num_batches=100 --weight_decay=1e-4 --use_fp16 \
        --display_every=10
 
 #Kill logger
