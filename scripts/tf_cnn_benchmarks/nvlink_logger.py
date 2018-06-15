0de5023f442b75c17ddb734fe9810b4565d09a3a
Randall Pittman <randall_pittman@bellsouth.net>
2018-06-15 11:06:30 -0400

    papers

--- scripts/tf_cnn_benchmarks/nvlink_logger.py
+++ scripts/tf_cnn_benchmarks/nvlink_logger.py
@@ -1,15 +1,15 @@
 import os, sys, time
 
 def get_link_util():
-    total_rx_kb = 0
-    total_tx_kb = 0
-    count = 0
+    # total_rx_kb = 0
+    # total_tx_kb = 0
+    # count = 0
     for line in os.popen("nvidia-smi nvlink -g 0").readlines():
         line = line.strip()
         if "GPU" != line[:3]:
             line_data = line.split(' ')
-            total_rx_kb += int(line_data[3])
-            total_tx_kb += int(line_data[-2])
+            # total_rx_kb += int(line_data[3])
+            # total_tx_kb += int(line_data[-2])
             count += 1
     avg_rx_kb = total_rx_kb / float(count)
     avg_tx_kb = total_tx_kb / float(count)
