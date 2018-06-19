a32d42a915d2e4b236dfcdfe7d9def2a1b70a05b
Randall <randall_pittman@bellsouth.net>
2018-06-19 19:02:13 +0000

    advanced

--- run_ps.sh
+++ run_ps.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-python3 tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=2 \
-       --batch_size=32 --model=resnet50 --variable_update=distributed_replicated \
-       --job_name=ps --ps_hosts=172.31.8.57:50000,172.31.7.145:50000 \
-       --worker_hosts=172.31.8.57:50001,172.31.7.145:50001 --task_index=0
+H1=172.31.91.238
+H2=172.31.93.217
+
+
+ifconfig | grep ${H1} > /dev/null
+if [ "$?" -eq 0 ]; then
+    TASK_INDEX=0;
+else
+    TASK_INDEX=1;
+fi
+
+run() {
+    python3 tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=2 \
+	    --batch_size=32 --model=resnet50 --variable_update=distributed_replicated \
+	    --job_name=${1} --ps_hosts=${H1}:50000,${H2}:50000 \
+	    --worker_hosts=${H1}:50001,${H2}:50001 --task_index=${TASK_INDEX}
+}
+
+run ps &
+run worker
+kill $(jobs -p)
