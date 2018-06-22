b24d3abd0469ec45a2e70429105c3eeb42e2716d
Ubuntu <ubuntu@ip-172-31-6-232.ec2.internal>
2018-06-22 15:50:47 +0000

    f

--- run.sh
+++ run.sh
@@ -22,4 +22,4 @@ fi
 
 echo "Job name: $JOB_NAME";
 echo "Task index: $TASK_INDEX";
-python3 tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=1 --batch_size=64 --model=resnet50 --variable_update=distributed_replicated --job_name=${JOB_NAME} --ps_hosts=${H1}:50000,${H2}:50000 --worker_hosts=${H1}:50001,${H2}:50001 --task_index=${TASK_INDEX} --allow_growth=1 --num_batches=200
+python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=1 --batch_size=64 --model=resnet50 --variable_update=distributed_replicated --job_name=${JOB_NAME} --ps_hosts=${H1}:50000,${H2}:50000 --worker_hosts=${H1}:50001,${H2}:50001 --task_index=${TASK_INDEX} --allow_growth=1 --num_batches=200
