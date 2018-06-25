cbbc19a434b3af5534e7baaa323481cb3102be22
Randall Pittman <rapittma@cisco.com>
2018-06-25 13:30:52 -0400

    f

---
+++ csv/plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import csv
+
+reader = csv.reader(open("nvlink_usage_H1.csv", 'r'))
+
+next(reader)
+data = [[float(x) for x in line] for line in reader]
+
+column = 1
+
+slope_data = []
+for i in range(1, len(data)):
+    entry1 = data[i-1]
+    entry2 = data[i  ]
+    elapsed_time = entry2[0] - entry1[0]
+    new_line = [entry2[0], (entry2[column] - entry1[column]) / elapsed_time]
+    slope_data.append(new_line)
+x, y1 = [[row[i] for row in slope_data] for i in range(2)]
+plt.plot(x, y1)
+plt.xlim()
+plt.show()
