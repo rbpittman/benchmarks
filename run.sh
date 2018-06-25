cebcc2057a5c1069e060d7648ab446148bf1a9d0
Ubuntu <ubuntu@ip-172-31-3-196.ec2.internal>
2018-06-25 12:00:18 +0000

    f

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 # Run the following commands on host_1 (10.0.0.2): 172.31.7.145
 
 #!/bin/bash
-H1=172.31.6.232
-H2=172.31.14.59
+H1=172.31.3.196
+H2=172.31.1.249
 
 if [ -z $1 ]; then
     echo "Need job name (worker/ps)";
@@ -22,4 +22,7 @@ fi
 
 echo "Job name: $JOB_NAME";
 echo "Task index: $TASK_INDEX";
-python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=1 --batch_size=64 --model=resnet50 --variable_update=distributed_replicated --job_name=${JOB_NAME} --ps_hosts=${H1}:50000,${H2}:50000 --worker_hosts=${H1}:50001,${H2}:50001 --task_index=${TASK_INDEX} --allow_growth=1 --num_batches=200
+
+echo "Starting net logger...";
+python3 net_logger.py 1 net_usage.csv &
+python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=4 --batch_size=128 --model=resnet50 --variable_update=distributed_replicated --job_name=${JOB_NAME} --ps_hosts=${H1}:50000,${H2}:50000 --worker_hosts=${H1}:50001,${H2}:50001 --task_index=${TASK_INDEX} --allow_growth=1 --num_batches=200
