7aeb36791b8542d8fbda1e87d6f3bef157aa4852
Randall Pittman <rapittma@cisco.com>
2018-06-26 09:34:06 -0400

    link data

--- csv/animate.py
+++ csv/animate.py
@@ -3,10 +3,12 @@
 # Takes link data as csv for 4X V100, animates links
 # TODO: Better desc
 ###
+from __future__ import print_function
 
 import turtle
 import csv
 import time
+from collections import Counter
 
 #delay in seconds
 DELAY = 1
@@ -14,14 +16,70 @@ DELAY = 1
 FACTOR = 150
 SQUARE_SIZE = 0.3
 
+#    0          1           2         3          4           5         6          7
 #GPU0_L0_rx,GPU0_L0_tx,GPU0_L1_rx,GPU0_L1_tx,GPU0_L2_rx,GPU0_L2_tx,GPU0_L3_rx,GPU0_L3_tx,
+#   8            9           10       11          12         13       14          15
 #GPU1_L0_rx,GPU1_L0_tx,GPU1_L1_rx,GPU1_L1_tx,GPU1_L2_rx,GPU1_L2_tx,GPU1_L3_rx,GPU1_L3_tx,
+#     16         17          18        19         20         21        22         23          24       25
 #GPU2_L0_rx,GPU2_L0_tx,GPU2_L1_rx,GPU2_L1_tx,GPU2_L2_rx,GPU2_L2_tx,GPU2_L3_rx,GPU2_L3_tx,GPU2_L4_rx,GPU2_L4_tx,
+#     26         27         28         29         30         31        32         33         34         35
 #GPU3_L0_rx,GPU3_L0_tx,GPU3_L1_rx,GPU3_L1_tx,GPU3_L2_rx,GPU3_L2_tx,GPU3_L3_rx,GPU3_L3_tx,GPU3_L4_rx,GPU3_L4_tx
+
+"""
+ID: index/column of dataset 
+GPU: GPU id
+Link: Link id for specific GPU
+Type: transmit (tx) or receive (rx)
+Conn: Connection ID for this link
+
+ID GPU Link Type Conn
+0  0 0 rx 31
+1  0 0 tx 30
+2  0 1 rx 19
+3  0 1 tx 18
+4  0 2 rx 9
+5  0 2 tx 8
+6  0 3 rx 35
+7  0 3 tx 34
+8  1 0 rx 5
+9  1 0 tx 4
+10 1 1 rx 29
+11 1 1 tx 28
+12 1 2 rx 17
+13 1 2 tx 16
+14 1 3 rx 23
+15 1 3 tx 22
+16 2 0 rx 13
+17 2 0 tx 12
+18 2 1 rx 3
+19 2 1 tx 2
+20 2 2 rx 27
+21 2 2 tx 26
+22 2 3 rx 15
+23 2 3 tx 14
+24 2 4 rx 33
+25 2 4 tx 32
+26 3 0 rx 21
+27 3 0 tx 20
+28 3 1 rx 11
+29 3 1 tx 10
+30 3 2 rx 1
+31 3 2 tx 0
+32 3 3 rx 25
+33 3 3 tx 24
+34 3 4 rx 7
+35 3 4 tx 6
+
+"""
+
+#It appears there is dual transmission involved. If there are two
+#links going to the same location, both are used in parallel with an
+#even split. For example, link indices 0 and 6 
+
 class V100X4:
     links = None #9 links, rx and tx, probably duplicated. 4X9=36 length list
     link_locs = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
-    link_labels = [
+    link_labels = []#TODO
     def __init__(self, data):
         self.data = data
         self.t = turtle.Turtle()
@@ -52,12 +110,24 @@ class V100X4:
         turtle.update()
     
     def animate(self):
-        for row in self.data:
-            self.links = row
-            self.draw()
-            time.sleep(DELAY)
-            
-        turtle.mainloop()
+        most_freq = []
+        for check_col in range(36):
+            cols = Counter()
+            for row_idx, row in enumerate(self.data):
+                self.links = row
+                max_dist = 100
+                value = row[check_col]
+                for i in range(len(row)):
+                    # print(row[i], value)
+                    if i != check_col and abs(row[i]-value) < max_dist:
+                        cols[i] += 1
+                # self.draw()
+                # time.sleep(DELAY)
+            common = cols.most_common(3)
+            most_freq.append([e[0] for e in common])
+        for i in range(len(most_freq)):
+            print(i, most_freq[i])
+            # turtle.mainloop()
         
 
 if __name__ == "__main__":
@@ -75,7 +145,8 @@ if __name__ == "__main__":
         new_line = []
         for column in range(2, 38):
             delta_col = (entry2[column] - entry1[column]) / elapsed_time
-            new_line.append((delta_col * 8000) / (10 ** 9))
+            # new_line.append((delta_col * 8000) / (10 ** 9))
+            new_line.append(delta_col)
         slope_data.append(new_line)
     v100s = V100X4(slope_data)
     v100s.animate()
