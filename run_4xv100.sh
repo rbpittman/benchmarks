8af08f65e15d8375c5dc4350cfd0c1460ffa263a
Ubuntu <ubuntu@ip-172-31-0-154.ec2.internal>
2018-06-28 18:09:33 +0000

    f

--- run_4xv100.sh
+++ run_4xv100.sh
@@ -3,10 +3,10 @@
 echo "Starting net logger...";
 # python3 net_logger.py 0.2 net_usage.csv &
 echo "Starting nvlink logger...";
-python3 nvlink_logger.py 0.5 nvlink_usage.csv & 
+python3 nvlink_logger.py 0.2 nvlink_usage.csv & 
 echo "Done"
 
-python tf_cnn_benchmarks.py --num_gpus=4 --batch_size=128 --model=resnet50 --variable_update=parameter_server
+python tf_cnn_benchmarks.py --num_gpus=4 --batch_size=128 --model=resnet50 --variable_update=parameter_server --data_dir=/home/ubuntu/imagenet
 
 # for id in $(jobs -rp); do
     # kill $id;
