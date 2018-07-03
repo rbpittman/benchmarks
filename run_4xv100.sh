dc76f84e46e4f126c75a45b8a548750757b23dae
Ubuntu <ubuntu@ip-172-31-9-94.ec2.internal>
2018-07-03 14:33:40 +0000

    f

--- run_4xv100.sh
+++ run_4xv100.sh
@@ -6,7 +6,7 @@ echo "Starting nvlink logger...";
 ./nvlink_logger 0 &
 echo "Done"
 
-MODEL=resnet50
+MODEL=resnet152_v2
 BS=64
 
 #python tf_cnn_benchmarks.py --num_gpus=4 --batch_size=64 --model=resnet152_v2 --variable_update=replicated --trace_file=trace.json
