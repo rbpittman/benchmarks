5b0673deaf73c7317b054dc3c3c2cc83bafac1cf
Randall Pittman <randall_pittman@bellsouth.net>
2018-07-03 10:26:24 -0400

    f

--- gca04_train.sh
+++ gca04_train.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #nvprof --unified-memory-profiling off --metrics dram_read_throughput
-python tf_cnn_benchmarks.py --num_gpus=2 --batch_size=16 --model=resnet152_v2 --data_dir=/home/rapittma/imagenet/tf_records
+MODEL=resnet50
+BS=16
+python tf_cnn_benchmarks.py --num_gpus=2 --batch_size=${BS} --model=${MODEL} --data_dir=/home/rapittma/imagenet/tf_records --summary_verbosity=0 --train_dir=temp_train_dir --trace_file=trace_${MODEL}_${BS}.json
