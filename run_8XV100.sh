9692208fbf07b4aff44bca45ba69500661233531
Ubuntu <ubuntu@ip-172-31-15-43.ec2.internal>
2018-07-09 18:57:37 +0000

    f

--- run_8XV100.sh
+++ run_8XV100.sh
@@ -4,16 +4,19 @@
 # python3 net_logger.py 0.2 net_usage.csv &
 
 
-# echo "Starting nvlink logger...";
-# ./nvlink_logger 0 &
-# echo "Done"
+echo "Starting nvlink logger...";
+./nvlink_logger 0 &
+echo "Done"
 
-BS=64;
-for model in resnet50 resnet152_v2 inception3 vgg19; do
-    echo "auto-log-parser-reset ===========================";
-    echo "LocalBS: $BS";
-    python tf_cnn_benchmarks.py --num_gpus=8 --batch_size=$BS --model=$model --variable_update=replicated --all_reduce_spec=nccl
-done
+#resnet152_v2: bs <= 64
+#resnet50    : bs <= 128
+
+BS=128;
+echo "auto-log-parser-reset ===========================";
+echo "LocalBS: $BS";
+python tf_cnn_benchmarks.py --num_gpus=8 --batch_size=$BS --model=resnet50 --variable_update=replicated
+
+#--all_reduce_spec=nccl
 
 #--data_dir=/home/ubuntu/imagenet
 
