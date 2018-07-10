d3d5daff8e61bec93cbd079348497ea779ed0f1d
Randall Pittman <rapittma@cisco.com>
2018-07-10 09:14:01 -0400

    upgraded mgbench parser

--- mgbench_scripts/parse.py
+++ mgbench_scripts/parse.py
@@ -14,13 +14,58 @@ trans_matrix = [[0, 1, 1, 1, 1, 2, 2, 2],
                 [2, 2, 1, 2, 1, 1, 0, 1],
                 [2, 2, 2, 1, 1, 1, 1, 0]]
 
+NUM_LINKS = [[0, 1, 1, 2, 2, 0, 0, 0],
+             [1, 0, 2, 1, 0, 2, 0, 0],
+             [1, 2, 0, 2, 0, 0, 1, 0],
+             [2, 1, 2, 0, 0, 0, 0, 1],
+             [2, 0, 0, 0, 0, 1, 1, 2],
+             [0, 2, 0, 0, 1, 0, 2, 1],
+             [0, 0, 1, 0, 1, 2, 0, 2],
+             [0, 0, 0, 1, 2, 1, 2, 0]]
+
+
 #Data size for which ms scaling should be used instead of us. 
 MS_DATA_SIZE = 100 * 2**20
 SHOW_HIST = False
 
-def is_one_hop(start_gpu, end_gpu):
-    global trans_matrix
-    return trans_matrix[start_gpu][end_gpu] == 1
+LAT_TEST_SIZES = [4, 102400, 1048576, 104857600]
+BW_TEST_SIZES = [5] + [10 ** i for i in range(1, 9)]
+
+class Entry:
+    #if is_dma, then dma is active.
+    #otherwise, p2p is active. 
+    def __init__(self, src_gpu, dest_gpu, MBps, lat_ms, data_size, is_dma):
+        self.src_gpu = src_gpu
+        self.dest_gpu = dest_gpu
+        self.MBps = MBps
+        self.lat_ms = lat_ms
+        self.data_size = data_size
+        self.is_dma = is_dma
+
+    def __str__(self):
+        return "GPU %d -> %d: bw=%f, lat_ms=%f, data_size=%d, dma=%s" % (self.src_gpu, self.dest_gpu, self.MBps, self.lat_ms, self.data_size, str(self.is_dma))
+
+    def is_one_hop(self):
+        global trans_matrix
+        return trans_matrix[self.src_gpu][self.dest_gpu] == 1
+
+    def num_links(self):
+        global NUM_LINKS
+        return NUM_LINKS[self.src_gpu][self.dest_gpu]
+
+    #Returns a tuple identifying the conditions for the test that
+    #the entry represents. 
+    def get_conditions(self):
+        # return (self.src_gpu, self.dest_gpu, self.data_size, self.is_dma)
+        return self.data_size
+
+    def is_bw_size_test(self):
+        global BW_TEST_SIZES
+        return self.data_size in BW_TEST_SIZES
+    
+# def is_one_hop(start_gpu, end_gpu):
+#     global trans_matrix
+#     return trans_matrix[start_gpu][end_gpu] == 1
 
 def cycle_to(s):
     global f, line
@@ -44,6 +89,112 @@ def get_col(matrix, col):
         raise Exception("Column %d out of range for matrix with %d columns" % (col, len(matrix[0])))
     return [row[col] for row in matrix]
 
+
+lines = f.readlines()
+line_i = 0
+
+#Contains a bunch of Entry objects
+all_data = []
+
+#Known that there are 3 sets of data
+for _ in range(3):
+    line = lines[line_i]
+    #Cycle to ======
+    while "======" not in line:
+        line_i += 1
+        line = lines[line_i]
+    
+    #next is "Size:" line 
+    line_i += 1
+    line = lines[line_i]
+    assert "Size:" in line
+    data_size = int(line.split(' ')[1])
+    #Skip to type of test
+    line_i += 2
+    line = lines[line_i]
+    is_dma = "DMA" in line
+
+    pattern = re.compile(r"Copying from GPU ([0-9]) to GPU ([0-9]): ([0-9]+\.[0-9]+) MB/s \(([0-9]+\.[0-9]+) ms\)")
+    while "======" not in line:
+        if "Size:" in line:
+            data_size = int(line.split(' ')[1])
+        else:
+            match = re.match(pattern, line.strip())
+            if match != None:
+                groups = match.groups()
+
+                src  = int(groups[0])
+                dest = int(groups[1])
+                MBps = float(groups[2])
+                lat_ms = float(groups[3])
+                all_data.append(Entry(src, dest, MBps, lat_ms, data_size, is_dma))
+        line_i += 1
+        line = lines[line_i]
+
+#Finds entries with identical condition variables (src_gpu, dest_gpu,
+#data_size, is_dma) and averages them.
+#Returns a list of new entries. These entries have their lat_ms and MBps
+#parameters averaged. 
+def average_dup_entries(entries, reduction_factor):
+    entries_dict = {}
+    for entry in entries:
+        conditions = entry.get_conditions()
+        if conditions not in entries_dict:
+            entries_dict[conditions] = [[], []]
+        entries_dict[conditions][0].append(entry.MBps)
+        entries_dict[conditions][1].append(entry.lat_ms)
+    unique_entries = []
+    for conditions, MBpss_and_lat_mss in entries_dict.items():
+        MBpss, lat_mss = MBpss_and_lat_mss
+        data_size = conditions
+        new_entry = Entry(None, None,
+                          mean(MBpss)/float(reduction_factor),
+                          mean(lat_mss)/float(reduction_factor), data_size, None)
+        unique_entries.append(new_entry)
+    unique_entries.sort(key=lambda e: e.data_size)
+    return unique_entries
+
+def get_bw(entries):
+    return [entry.MBps for entry in entries]
+
+def get_lat(entries):
+    return [entry.lat_ms for entry in entries]
+
+def plot_8a():
+    global all_data
+    all_my_data = [entry for entry in all_data if entry.is_bw_size_test()]
+    
+    #Grab all the entries for a given type, including duplicates
+    all_one_hop = [entry for entry in all_my_data if not entry.is_dma and entry.is_one_hop()]
+    all_one_hop_1link = [entry for entry in all_one_hop if entry.num_links() == 1]
+    all_one_hop_2link = [entry for entry in all_one_hop if entry.num_links() == 2]
+    all_two_hop = [entry for entry in all_my_data if not entry.is_dma and not entry.is_one_hop()]
+
+    #Remove duplicates
+    one_hop_1link = average_dup_entries(all_one_hop_1link, 2**10)
+    one_hop_2link = average_dup_entries(all_one_hop_2link, 2**10)
+    two_hop       = average_dup_entries(all_two_hop      , 2**10)
+    
+    x  = BW_TEST_SIZES
+    y_1hop_1link = get_bw(one_hop_1link)
+    y_1hop_2link = get_bw(one_hop_2link)
+    y_2hop       = get_bw(two_hop      )
+    
+    plt.plot(x, y_1hop_1link, 's' , color='orange', linestyle='--', linewidth=3, markersize=10, label="Inter GPU 1-hop (1 nvlink)")
+    plt.plot(x, y_1hop_2link, 'd' , color='orange', linestyle='--', linewidth=3, markersize=10, label="Inter GPU 1-hop (2 nvlinks)")
+    plt.plot(x, y_2hop      , 'bo', color='b'     , linestyle='--', linewidth=3, markersize=10, label="Inter GPU 2-hop")
+    
+    plt.legend(loc="upper left")
+    plt.xscale("log")
+    plt.xlabel("Data size (bytes)")
+    plt.ylabel("GiBps")
+    plt.title("GPU-to-GPU memory copy (No DMA)")
+    # plt.title("GPU-to-GPU DMA")
+    plt.show()
+
+plot_8a()
+
+"""
 def parse_bw(is_dma):
     global f, line
     cycle_to("======")
@@ -123,10 +274,12 @@ def parse_lat():
 # fig8a = parse_lat(False)
 # fig8b = parse_bw(True)
 # fig9  = parse_lat()
+"""
 
 #parse_lat returns list of [data_size, data] where data_size is the
 #size of the data being transferred, and data is a 2D list with
 #entries of the form [gpu_dest, latency]
+"""
 p2p_memcpy_data = parse_lat()
 dma_data = parse_lat()
 assert(len(p2p_memcpy_data) == len(dma_data))
@@ -156,6 +309,7 @@ for i in range(len(p2p_memcpy_data)):
     #     plt.ylabel('Latency, us')
     plt.show()
 
+"""
 """
 #Plot figure 8a (and 8b if re-commented correctly)
 x  = get_col(fig8b, 0)
