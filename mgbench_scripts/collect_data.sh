fe8824cc0caf318b117b4133071c133c3252ce90
Ubuntu <ubuntu@ip-172-31-1-114.ec2.internal>
2018-07-11 12:39:33 +0000

    f

--- mgbench_scripts/collect_data.sh
+++ mgbench_scripts/collect_data.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 cd ~/mgbench
 
-BW_SIZES="5 10 100 1000 10000 100000 1000000 10000000 100000000";
-LATENCY_SIZES="4 102400 1048576 104857600"; #4byte, 100KB, 1MB, 100MB
+# BW_SIZES="5 10 100 1000 10000 100000 1000000 10000000 100000000";
+# LATENCY_SIZES="4 102400 1048576 104857600"; #4byte, 100KB, 1MB, 100MB
 
-run_exec() {
-    for size in $2; do
-	echo "Size: $size bytes";
-	echo "------------------------";
-	$1 -size $size;
-	echo "";
-    done
-}
+# run_exec() {
+#     for size in $2; do
+# 	echo "Size: $size bytes";
+# 	echo "------------------------";
+# 	$1 -size $size;
+# 	echo "";
+#     done
+# }
 
-echo "Figure 8a";
-echo "=========";
-run_exec ./build/halfduplex "$BW_SIZES"
+# echo "Figure 8a";
+# echo "=========";
+# run_exec ./build/halfduplex "$BW_SIZES"
 
-echo "Figure 8b";
-echo "=========";
-run_exec ./build/uva "$BW_SIZES"
+# echo "Figure 8b";
+# echo "=========";
+# run_exec ./build/uva "$BW_SIZES"
 
-echo "Figure 9";
-echo "=========";
-run_exec ./build/halfduplex "$LATENCY_SIZES"
+# echo "Figure 9";
+# echo "=========";
+# run_exec ./build/halfduplex "$LATENCY_SIZES"
+
+/home/ubuntu/benchmarks/nvlink_logger 0 &
+
+./build/halfduplex -size 10737418240 -repetitions 1
+
+kill `jobs -rp`;
 
 exit 0;
