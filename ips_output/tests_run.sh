f28dfa7fe14ab5a5b4d7643434a22d12ece78fa3
Ubuntu <ubuntu@ip-172-31-7-228.ec2.internal>
2018-07-12 14:15:22 +0000

    f

--- ips_output/tests_run.sh
+++ ips_output/tests_run.sh
@@ -35,3 +35,17 @@ for model in resnet50 resnet152_v2 inception3 vgg19; do
     python tf_cnn_benchmarks.py --num_gpus=8 --batch_size=$BS --model=$model --variable_update=replicated --all_reduce_spec=nccl
 done
 #resnet152_v2 crashes for BS128. Reducing to 64
+
+
+#inception3_bs128_with_fp16_testing_num_gpus.out
+BS=128;
+for NUM_GPUS in 1 2 4 8; do
+    echo "auto-log-parser-reset ===========================";
+    echo "LocalBS: $BS";
+    python tf_cnn_benchmarks.py --num_gpus=$NUM_GPUS --batch_size=$BS --model=inception3 --variable_update=replicated --all_reduce_spec=nccl --use_fp16=1
+done
+
+
+
+#resnet50_bs256_8XV100_with_fp16.out
+python tf_cnn_benchmarks.py --num_gpus=8 --batch_size=256 --model=resnet50 --variable_update=replicated --all_reduce_spec=nccl --use_fp16=1
