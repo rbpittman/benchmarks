f73828fb899799fd0f6c42b40585df73f544b055
Ubuntu <ubuntu@ip-172-31-7-228.ec2.internal>
2018-07-12 15:19:33 +0000

    f

--- run_8XV100.sh
+++ run_8XV100.sh
@@ -11,7 +11,10 @@ echo "Done"
 #resnet152_v2: bs <= 64
 #resnet50    : bs <= 128
 
-python tf_cnn_benchmarks.py --num_gpus=8 --batch_size=128 --model=resnet50 --variable_update=replicated --all_reduce_spec=nccl --use_fp16=1 --gradient_repacking=1 --trace_file=traces/resnet50_bs128_8XV100_with_nccl_with_fp16_with_grad_repack_1.json
+python tf_cnn_benchmarks.py --num_gpus=8 --batch_size=128 --model=resnet50 --variable_update=replicated --all_reduce_spec=nccl --gradient_repacking=1
+
+#--use_fp16=1
+#--trace_file=traces/resnet50_bs128_8XV100_with_nccl_with_fp16_with_grad_repack_1.json
 
 #--all_reduce_spec=nccl
 
